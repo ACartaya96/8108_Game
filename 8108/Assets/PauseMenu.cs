e28c978c9e958530d29be5cf2e822669720e2dde
LuisEstades <86684078+LuisEstades@users.noreply.github.com>
2022-03-04 16:11:52 -0500

    Pause Menu
    -Moved  MainMenu Script into Script folder
    -Panel for pause menu added to Canvas
    -Made Script for Pause Menu
    -Menu buttons function.

---
+++ 8108/Assets/PauseMenu.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+    CursorLockMode desiredMode;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+
+
+
+    }
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Cursor.visible = false;
+        desiredMode = CursorLockMode.Confined;
+
+    }
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Cursor.visible = true;
+        desiredMode = CursorLockMode.None;
+        {
+            Cursor.lockState = desiredMode;
+        }
+    }
+
+    public void LoadMenu()
+    {
+        Debug.Log("loading menu...");
+        SceneManager.LoadScene("8108 Main Menu");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("quit!!!");
+        Application.Quit();
+    }
+}
+
